4f6353c9ab68c6fdab2d37ea4b695fd379248ffd
Aditya <adityang5@gmail.com>
2021-04-09 12:32:55 +0530

    Loaded adjL into memory

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct node {
+	int data;
+	struct node* link;
+} node_t;
 
 int main() {
 	FILE * fp;
@@ -13,25 +17,48 @@ int main() {
 	if (fp == NULL)
 		exit(EXIT_FAILURE);
 	
-	while ((read = getline(&line, &len, fp)) != -1) {
-		//printf("Retrieved line of length %zu:\n", read);
-		//printf("%s", line);
+	int n;
+	fscanf(fp, "%d\n", &n);
+	node_t *adjL = (node_t*) malloc(sizeof(node_t) * n);
 
+	int count = 0;
+
+	while ((read = getline(&line, &len, fp)) != -1) {
 		char * token = strtok(line, " ");
-		// loop through the string to extract all other tokens
+		
+		node_t* head = &adjL[count];
+		node_t* p = head;
+		// loop through the line to extract all other tokens
 		while( token != NULL ) {
 			int d;
 			sscanf(token, "%d", &d);
-			printf( " %d ", d ); //printing each token
+			//printf( " %d ", d ); //printing each token
 			token = strtok(NULL, " ");
+
+			p->data = d;
+			p->link = malloc(sizeof(node_t));
+			p = p->link;
+
 		}
-		printf("\n");
+		//printf("\n");
+		p->link = NULL;
+		count++;
 	}
 	
 	fclose(fp);
 	if (line)
 		free(line);
 	
+	for (int i=0; i<n; i++) {
+		printf("%d -> ", i);
+		node_t* p = &adjL[i];
+		while (p != NULL) {
+			printf("%d ", p->data);
+			p = p->link;
+		}
+		printf("\n");
+	}
+
 	exit(EXIT_SUCCESS);
 	return 0;
 }
